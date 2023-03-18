1dbade9a8dd83842addcda5652a91a26467b1df9
agart001 <agart.art@gmail.com>
2023-03-18 15:15:46 -0500

    com

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,6 @@ using namespace std;
 
 int main()
 {
-    Card MyCard(5, "Hearts");
-    MyCard.PrintInfo();
+    cout << "hello world" << endl;
 }
 
-class Card
-{
-private:
-
-public:
-
-int Number;
-
-string Suite;
-
-
-Card(int _num, string _suite){
-    Number = _num;
-    Suite = _suite;
-}
-
-void PrintInfo()
-{
-    cout << "Num: " + Number << endl;
-    cout << "Suite: " + Suite << endl;
-}
-
-};
